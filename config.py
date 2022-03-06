8572206cb8051512d273605ce37cc8a185b9afdd
Jelle Vegter <jelle.vegter@nederlandseloterij.nl>
2022-03-06 14:01:50 +0100

    new conn string

--- config.py
+++ config.py
@@ -5,4 +5,17 @@
 # export ARM_SUBSCRIPTION_ID="40d5fdc2-f354-475d-b689-9525849a6bd9"
 # export ARM_TENANT_ID="1909a595-5f96-4b0c-9d43-df6ca3afa9ab"
 
-# export ADLS_NBA_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=fantasynba;AccountKey=3t8kTH2U+e+0jPHuv7Am0+zW8GN4BGdeMflNtrDo0ctvS8y9YKECTkZyucghW029ls7a8fn35le1DPrztIqJ0Q==;EndpointSuffix=core.windows.net"
+export ADLS_NBA_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=fantasynba;AccountKey=HZlBM0HPf89moIt/32c1Z/NbiIYUoZSl4BTjT7KsT2NJhOUDpEwQIM+NFqJ8YOBv6uV7aTy4aGnbz3G9t1qCcg==;EndpointSuffix=core.windows.net"
+
+#   AZURE_TENANT_ID = '1909a595-5f96-4b0c-9d43-df6ca3afa9ab'
+
+
+# Storage acc Snowlake
+# AZURE_MULTI_TENANT_APP_NAME = "SnowflakePACInt1397_1646497111088"
+# STORAGE_ALLOWED_LOCATIONS = ('azure://fantasynba.blob.core.windows.net/gamesplayed/');
+
+# Queue acc Snowflake
+# AZURE_MULTI_TENANT_APP_NAME = SnowflakePACInt1400_1646502127932
+# STORAGE_ALLOWED_LOCATIONS = ('azure://fantasynba.queue.core.windows.net/snowflakequeue')
+
+
